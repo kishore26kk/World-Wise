3f708df04274ac6583657a4c0da73183bfa198de
Kishore Kumar <iamkk26073@gmail.com>
2023-08-08 12:31:09 +0530

    changes in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,39 @@
+import { Suspense, lazy } from 'react';
 import {BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import Product from './pages/Product'
-import Pricing from './pages/Pricing'
-import Homepage from './pages/Homepage'
-import PageNotFound from './pages/PageNotFound'
-import Login from './pages/Login'
-import AppLayout from './pages/AppLayout'
+
+import { CitiesProvider } from './context/CitiesContext';
+import {AuthProvider} from './context/FakeAuthContext';
+import ProtectedRoute from './pages/ProtectedRoute';
+
+// import Product from './pages/Product'
+// import Pricing from './pages/Pricing'
+// import Homepage from './pages/Homepage'
+// import Login from './pages/Login'
+// import AppLayout from './pages/AppLayout'
+// import PageNotFound from './pages/PageNotFound'
+
+const Homepage = lazy(()=> import('./pages/Homepage'))
+const Product = lazy(()=> import('./pages/Product'))
+const Pricing = lazy(()=> import('./pages/Pricing'))
+const Login = lazy(()=> import('./pages/Login'))
+const AppLayout = lazy(()=> import('./pages/AppLayout'))
+const PageNotFound = lazy(()=> import('./pages/PageNotFound'))
+
+// dist/assets/index-5964b7f9.css   30.15 kB │ gzip:   5.06 kB
+// dist/assets/index-421822a1.js   524.71 kB │ gzip: 148.72 kB
+
 import CityList from './components/CityList'
 import CountryList from './components/CountryList'
 import City from './components/City'
 import Form from './components/Form'
-import { CitiesProvider } from './context/CitiesContext';
-import {AuthProvider} from './context/FakeAuthContext';
-import ProtectedRoute from './pages/ProtectedRoute'
+import SpinnerFullPage from './components/SpinnerFullPage';
 
 function App() {
   return (
     <AuthProvider>
     <CitiesProvider>
     <BrowserRouter>
+    <Suspense fallback={<SpinnerFullPage />}>
     <Routes>
       <Route index element={<Homepage />} />
       <Route path="product" element={<Product />} />
@@ -32,6 +48,7 @@ function App() {
       <Route path="login" element={<Login />}/>
       <Route path="*" element={<PageNotFound />}/>
     </Routes>
+    </Suspense>
     </BrowserRouter>
     </CitiesProvider>
     </AuthProvider>
--- src/context/CitiesContext.jsx
+++ src/context/CitiesContext.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react-refresh/only-export-components */
 /* eslint-disable react/prop-types */
-import { useEffect, createContext, useContext, useReducer } from "react";
+import { useEffect, createContext, useContext, useReducer, useCallback } from "react";
 
 const CitiesContext = createContext();
 const BASE_URL = 'http://localhost:8000'
@@ -77,7 +77,7 @@ const CitiesProvider = ({children}) =>{
     fetchCities()
   },[]) 
 
-  async function getCity(id){
+  const getCity = useCallback(async function getCity(id){
     if(+id === currentCity.id) return;
     
     dispatch({type:"loading"});
@@ -89,7 +89,7 @@ const CitiesProvider = ({children}) =>{
       catch{
         dispatch({type: "rejected", payload : 'There was an error loading the city...'})
       }
-  }
+  },[currentCity.id])
 
   async function createCity(newCity){
     dispatch({type : "loading"})
